f0e24fa5d9cb4c46956e5fc5684b7554e1a3a24a
beejpersson <>
2017-10-30 16:43:41 +0000

    Updated git ignore, 2 initial tests done. Results file added.

--- Coursework Part 1/Sequential Ray Tracer/raytracer.cpp
+++ Coursework Part 1/Sequential Ray Tracer/raytracer.cpp
@@ -296,9 +296,12 @@ int main(int argc, char **argv)
     uniform_real_distribution<double> distribution;
     auto get_random_number = bind(distribution, generator);
 
+	// Create results file
+	ofstream results("data.csv", ofstream::out);
+
     // *** These parameters can be manipulated in the algorithm to modify work undertaken ***
-    constexpr size_t dimension = 1024;
-    constexpr size_t samples = 16; // Algorithm performs 4 * samples per pixel.
+    constexpr size_t dimension = 256;
+    constexpr size_t samples = 2; // Algorithm performs 4 * samples per pixel.
     vector<sphere> spheres
     {
         sphere(1e5, vec(1e5 + 1, 40.8, 81.6), vec(), vec(0.75, 0.25, 0.25), reflection_type::DIFFUSE),
@@ -319,28 +322,40 @@ int main(int argc, char **argv)
     vec r;
     vector<vec> pixels(dimension * dimension);
 
-    for (size_t y = 0; y < dimension; ++y)
-    {
-        cout << "Rendering " << dimension << " * " << dimension << "pixels. Samples:" << samples * 4 << " spp (" << 100.0 * y / (dimension - 1) << ")" << endl;
-        for (size_t x = 0; x < dimension; ++x)
-        {
-            for (size_t sy = 0, i = (dimension - y - 1) * dimension + x; sy < 2; ++sy)
-            {
-                for (size_t sx = 0; sx < 2; ++sx)
-                {
-                    r = vec();
-                    for (size_t s = 0; s < samples; ++s)
-                    {
-                        double r1 = 2 * get_random_number(), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-                        double r2 = 2 * get_random_number(), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-                        vec direction = cx * static_cast<double>(((sx + 0.5 + dx) / 2 + x) / dimension - 0.5) + cy * static_cast<double>(((sy + 0.5 + dy) / 2 + y) / dimension - 0.5) + camera.direction;
-                        r = r + radiance(spheres, ray(camera.origin + direction * 140, direction.normal()), 0) * (1.0 / samples);
-                    }
-                    pixels[i] = pixels[i] + vec(clamp(r.x, 0.0, 1.0), clamp(r.y, 0.0, 1.0), clamp(r.z, 0.0, 1.0)) * 0.25;
-                }
-            }
-        }
-    }
+	results << "Test, Image Dimensions, Samples Per Pixel, Time, " << endl;
+
+	for (unsigned int i = 0; i < 100; ++i)
+	{
+		// **** TIME FROM HERE... ****
+		auto start = system_clock::now();
+		for (size_t y = 0; y < dimension; ++y)
+		{
+			//cout << "Rendering " << dimension << " * " << dimension << "pixels. Samples:" << samples * 4 << " spp (" << 100.0 * y / (dimension - 1) << ")" << endl;
+			for (size_t x = 0; x < dimension; ++x)
+			{
+				for (size_t sy = 0, i = (dimension - y - 1) * dimension + x; sy < 2; ++sy)
+				{
+					for (size_t sx = 0; sx < 2; ++sx)
+					{
+						r = vec();
+						for (size_t s = 0; s < samples; ++s)
+						{
+							double r1 = 2 * get_random_number(), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
+							double r2 = 2 * get_random_number(), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
+							vec direction = cx * static_cast<double>(((sx + 0.5 + dx) / 2 + x) / dimension - 0.5) + cy * static_cast<double>(((sy + 0.5 + dy) / 2 + y) / dimension - 0.5) + camera.direction;
+							r = r + radiance(spheres, ray(camera.origin + direction * 140, direction.normal()), 0) * (1.0 / samples);
+						}
+						pixels[i] = pixels[i] + vec(clamp(r.x, 0.0, 1.0), clamp(r.y, 0.0, 1.0), clamp(r.z, 0.0, 1.0)) * 0.25;
+					}
+				}
+			}
+		}
+		auto end = system_clock::now();
+		// **** ...TO HERE ****
+		auto total = duration_cast<milliseconds>(end - start).count();
+		results << i + 1 << ", " << dimension << ", " << samples * 4 << ", " << total << endl;
+	}
+
     cout << "img.bmp" << (array2bmp("img.bmp", pixels, dimension, dimension) ? " Saved\n" : " Save Failed\n");
     return 0;
 }
