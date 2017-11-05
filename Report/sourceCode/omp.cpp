8ee8c08045a14794cb40d12eba0d0d592114e627
Beej Persson <beejpersson@btinternet.com>
2017-11-05 12:28:19 +0000

    Finished methodology section, starting writing results, updated code snippets.

--- Report/sourceCode/omp.cpp
+++ Report/sourceCode/omp.cpp
@@ -1,5 +1,3 @@
-    auto num_threads = thread::hardware_concurrency();
-    int y;
 #pragma omp parallel for num_threads(num_threads) private(y, r) schedule(dynamic)
     for (y = 0; y < dimension; ++y)
     {
