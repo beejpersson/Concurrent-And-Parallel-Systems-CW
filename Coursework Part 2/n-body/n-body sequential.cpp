125157bb6db0b9238b956675376f6f1e83a909e6
beejpersson <>
2017-11-25 17:06:42 +0000

    Added json output to be visualised in python.

--- Coursework Part 2/n-body/n-body sequential.cpp
+++ Coursework Part 2/n-body/n-body sequential.cpp
@@ -3,6 +3,10 @@
 #include <cmath>
 #include <chrono>
 #include <fstream>
+#include <vector>
+#include <stdio.h>
+#include <errno.h>
+
 
 // Small value to avoid dividing by zero in force calculation
 #define SOFTENING 1e-9f
@@ -24,15 +28,15 @@ struct MVect3 {
 		z = nz;
 	}
 	void zero() {
-		x = 0.0;
-		y = 0.0;
-		z = 0.0;
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
 	}
 };
 
 // Body struct - with position, velocity, mass and constructor
 struct MutableBody {
-	MVect3 p = { 0,0,0 }, v = { 0,0,0 };
+	MVect3 p = { 0.0f,0.0f,0.0f }, v = { 0.0f,0.0f,0.0f };
 	float mass;
 	MutableBody() {}
 	MutableBody(const MVect3 &np, const MVect3 &nv, float m) {
@@ -42,8 +46,112 @@ struct MutableBody {
 	}
 };
 
+// ** FAILED ATTEMPT AT CREATING BMP IMAGE FROM POSITION DATA **
+//struct lwrite
+//{
+//	unsigned long value;
+//	unsigned size;
+//
+//	lwrite(unsigned long value, unsigned size) noexcept
+//		: value(value), size(size)
+//	{
+//	}
+//};
+//
+//inline std::ostream &operator<<(std::ostream &outs, const lwrite &v)
+//{
+//	unsigned long value = v.value;
+//	for (unsigned cntr = 0; cntr < v.size; cntr++, value >>= 8)
+//		outs.put(static_cast<char>(value & 0xFF));
+//	return outs;
+//}
+//
+//bool array2bmp(const std::string &filename, const vector<MVect3> &pixels, const size_t width, const size_t height)
+//{
+//	std::ofstream f(filename.c_str(), std::ios::out | std::ios::trunc | std::ios::binary);
+//	if (!f)
+//	{
+//		return false;
+//	}
+//	// Write Bmp file headers
+//	const size_t headers_size = 14 + 40;
+//	const size_t padding_size = (4 - ((height * 3) % 4)) % 4;
+//	const size_t pixel_data_size = width * ((height * 3) + padding_size);
+//	f.put('B').put('M'); // bfType
+//						 // bfSize
+//	f << lwrite(headers_size + pixel_data_size, 4);
+//	// bfReserved1, bfReserved2
+//	f << lwrite(0, 2) << lwrite(0, 2);
+//	// bfOffBits, biSize
+//	f << lwrite(headers_size, 4) << lwrite(40, 4);
+//	// biWidth,  biHeight,  biPlanes
+//	f << lwrite(width, 4) << lwrite(height, 4) << lwrite(1, 2);
+//	// biBitCount, biCompression = BI_RGB ,biSizeImage
+//	f << lwrite(24, 2) << lwrite(0, 4) << lwrite(pixel_data_size, 4);
+//	// biXPelsPerMeter, biYPelsPerMeter, biClrUsed, biClrImportant
+//	f << lwrite(0, 4) << lwrite(0, 4) << lwrite(0, 4) << lwrite(0, 4);
+//	// Write image data
+//	for (size_t x = height; x > 0; x--)
+//	{
+//		for (size_t y = 0; y < width; y++)
+//		{
+//			const auto &val = pixels[((x - 1) * width) + y];
+//			f.put(static_cast<char>(int(255.0 * val.z))).put(static_cast<char>(int(255.0 * val.y))).put(static_cast<char>(255.0 * val.x));
+//		}
+//		if (padding_size)
+//		{
+//			f << lwrite(0, padding_size);
+//		}
+//	}
+//	return f.good();
+//}
+// ** FAILED ATTEMPT AT CREATING BMP IMAGE FROM POSITION DATA **
+
+// ** FAILED PGM METHOD **
+//void writePGM(const char *filename, const PGMData *data)
+//{
+//	FILE *pgmFile;
+//	int i, j;
+//	int hi, lo;
+//
+//	pgmFile = fopen(filename, "wb");
+//	if (pgmFile == NULL) {
+//		perror("cannot open file to write");
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	fprintf(pgmFile, "P5 ");
+//	fprintf(pgmFile, "%d %d ", data>col, data->row);
+//	fprintf(pgmFile, "%d ", data->max_gray);
+//
+//	if (data->max_gray > 255) {
+//		for (i = 0; i < data->row; ++i) {
+//			for (j = 0; j < data->col; ++j) {
+//				hi = HI(data->matrix[i][j]);
+//				lo = LO(data->matrix[i][j]);
+//				fputc(hi, pgmFile);
+//				fputc(lo, pgmFile);
+//			}
+//
+//		}
+//	}
+//	else {
+//		for (i = 0; i < data->row; ++i) {
+//			for (j = 0; j < data->col; ++j) {
+//				lo = LO(data->matrix[i][j]);
+//				fputc(lo, pgmFile);
+//			}
+//		}
+//	}
+//
+//	fclose(pgmFile);
+//	deallocate_dynamic_matrix(data->matrix, data->row);
+//}
+// **
+
 // Body class - with initialise method and simulation method
 class NBodyMutableClass {
+private:
 	int numBodies;
 	float dt;
 	valarray<MutableBody> bodies;
@@ -74,6 +182,14 @@ public:
 	}
 
 	void forSim(int steps) {
+
+		FILE* rdata;
+		errno_t err;
+		if ((err = fopen_s(&rdata, "rdata.py", "w")) != 0) {
+			fprintf(stderr, "Couldn't open render.json");
+		}
+		fprintf(rdata, "data = [\n");
+
 		for (int step = 0; step < steps; ++step) {
 			for (int i = 0; i < numBodies; ++i) {
 				accel[i].zero();
@@ -108,6 +224,7 @@ public:
 					accel[i].x += dx * invDist3; accel[i].y += dy * invDist3; accel[i].z += dz * invDist3;*/
 				}
 			}
+			fprintf(rdata, "\t[");
 			for (int i = 0; i < numBodies; ++i) {
 				MutableBody & p = bodies[i];
 				// Before positions updated, print to results file
@@ -119,15 +236,22 @@ public:
 				p.p.x += p.v.x*dt;
 				p.p.y += p.v.y*dt;
 				p.p.z += p.v.z*dt;
+				fprintf(rdata, "[%f, %f, %f],", p.p.x, p.p.y, p.p.z);
+				
+				fprintf(stderr, "Finished iterations %d.\n", i);
 			}
+			fprintf(rdata, "],\n");
+			//array2bmp("img.bmp", positions, 1000, 1000);
 		}
+		fprintf(rdata, "]");
+		fclose(rdata);
 	}
 };
 
 int main(int argc, char *argv[]) {
 	using namespace std::chrono;
 
-	NBodyMutableClass sim(20, 0.01);
+	NBodyMutableClass sim(20, 0.01f);
 
 	results << "Body, Pos x, Pos y, Pos z, , Vel x, Vel y, Vel z" << endl;
 	//results << "Accel[i] x, y, z, , Accel[j] x, y, z" << endl;
@@ -140,11 +264,8 @@ int main(int argc, char *argv[]) {
 
 	duration<float> time_span = duration_cast<duration<float>>(t2 - t1);
 
-
-
 	std::cout << "It took me " << time_span.count() << " seconds.\n";
 	results << "\nTotal Time: ," << time_span.count() << endl;
-
 
 	return 0;
 }
---
+++ Coursework Part 2/n-body/video.py
@@ -0,0 +1,56 @@
+import sys
+import ctypes
+from sdl2 import *
+from sdl2.sdlgfx import *
+
+import rdata
+
+def main():
+    renderIdx = 0
+    renderLen = len(rdata.data)
+    
+    SDL_Init(SDL_INIT_VIDEO)
+    window = SDL_CreateWindow(
+        b"Hello World",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800,800,
+        SDL_WINDOW_SHOWN
+    )
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC
+    )
+
+    running = True
+    event = SDL_Event()
+    while running:
+        while SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == SDL_QUIT:
+                running = False
+                break
+            if event.type == SDL_KEYDOWN:
+                if event.key.keysym.sym == SDLK_q: running = False; break;
+                if event.key.keysym.sym == SDLK_SPACE: running = False; break;
+                if event.key.keysym.sym == SDLK_RETURN: running = False; break;
+                if event.key.keysym.sym == SDLK_ESCAPE: running = False; break;
+
+        SDL_SetRenderDrawColor(renderer, 0,0,0, 255)
+        SDL_RenderClear(renderer);
+
+        print renderIdx % renderLen
+        for body in rdata.data[renderIdx % renderLen]:
+            filledCircleRGBA(
+                renderer,
+                body[0]+400, body[1]+400, body[2],
+                255,0,255, 255
+            )
+        SDL_RenderPresent(renderer)
+        renderIdx += 1
+
+    SDL_DestroyWindow(window)
+    SDL_Quit()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
